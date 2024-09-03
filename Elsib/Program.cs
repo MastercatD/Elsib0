18508fd94d4093d2a8cefb79a27d4fbb5687572e
Быков Леонид <leniktyz@list.ru>
2024-09-03 14:25:24 +0700

    init and done

---
+++ Elsib/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elsib
+{
+		class Program
+		{
+
+				static void Main(string[] args)
+				{
+						int[] array1 = { 1, 2, 4, 5, 6, 8, 10 };
+						//	1 задача
+						Console.WriteLine("Task 1:");
+						Console.WriteLine(string.Join(", ", Tasks.task1(array1)));
+						//	2 задача
+						Tasks.task2(array1);
+						Console.WriteLine("Task 2:");
+						Console.WriteLine(string.Join(", ", Tasks.task2(array1)));
+						//	3 задача
+						string str = "Hello morld!";
+						Console.WriteLine("Task 3:");
+						char oldSym = 'm', newSym = 'w';
+						Console.WriteLine(Tasks.task3(str, oldSym, newSym));
+				}
+		}
+}
---
+++ Elsib/Tasks.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elsib
+{
+		class Tasks
+		{
+
+				public static int [] task1(int [] array)
+				{
+						List<int> resultList = new List<int>();
+						for (int i = 0, j = 1; i < array.Length; i++, j++)
+						{
+								while (array[i] > j)
+								{
+										resultList.Add(j);
+										j++;
+								}
+						}
+						return resultList.ToArray();
+				}
+
+				public static int[] task2(int [] array)
+				{
+						int [] result = new int[array.Length];
+						for (int i = 0; i < array.Length; result[i] = (array[i] % 2 == 0) ? 1 : 0, i++) ;
+						return result;
+				}
+				public static string task3(string str, char oldSym, char newSym)
+				{
+						int index = str.IndexOf(oldSym);
+						return newSym + str.Substring(index + 1);
+				}
+		}
+}
